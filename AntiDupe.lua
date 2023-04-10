a896c367c4e0361888f4d4f27cbecf138a936b78
zShadowSkilled <103582788+zShadowSkilled1@users.noreply.github.com>
2023-04-10 18:29:23 +0200

    Update AntiDupe.lua

--- AntiDupe.lua
+++ AntiDupe.lua
@@ -1,5 +1,21 @@
-local whitelist = {
-  "Player1",
-  "Player2",
-  "Player3"
-}
+-- Define the list of whitelisted players
+local whitelist = {"Player1", "Player2", "Player3"}
+
+-- Get the player's name
+local playerName = game.Players.LocalPlayer.Name -- Replace with whatever method you use to get the player's name
+
+-- Check if the player is whitelisted
+local isWhitelisted = false
+for i, name in ipairs(whitelist) do
+  if name == playerName then
+    isWhitelisted = true
+    break
+  end
+end
+
+-- Print a message depending on whether the player is whitelisted or not
+if isWhitelisted then
+  return true
+else
+  return false
+end
